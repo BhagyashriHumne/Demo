d8bc30bd8eaff1b339da9125417558779820f328
bhagyashriHumne <bhagihumne456@gmail.com>
2021-08-02 22:33:57 +0530

    Rest Maven Project for tomcat Integration

---
+++ src/main/java/com/jaxrs/practice/MyRestApiApp.java
@@ -0,0 +1,17 @@
+package com.jaxrs.practice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webapi")
+public class MyRestApiApp extends Application {
+
+	/*
+	 * @Override public Set<Class<?>> getClasses() { // TODO Auto-generated method
+	 * stub return new HashSet<Class<?>>(); }
+	 */
+
+}
